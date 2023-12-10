792d1bd667d2a2c91ceb3695085b30edb46f42c7
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-12-10 21:04:30 +0530

    code refactoring and enable helmet middleware to secure the nodejs http response header

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 const routes = express.Router();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const { authRouter, newsAggregatorRouter } = require('./routes');
 const { PORT_NUMBER } = require('./config/env.config');
 const { requestLoggerMiddleware } = require('./middlewares');
+const {
+  MSG_SERVER_LISTING,
+  ERROR_EVENT,
+  ERR_SERVER_START,
+} = require('./constants/app.constant');
+const helmet = require('helmet');
 
 const PORT = PORT_NUMBER || 3000;
 
 /* Middlewares */
+// enabling the helmet middleware
+app.use(helmet());
 app.use(requestLoggerMiddleware);
-app.use(bodyParser.json());
 app.use(routes);
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 /* Routers */
 routes.get('/', (req, res) => {
@@ -26,11 +36,13 @@ routes.use('/auth', authRouter);
 /* News Aggregator Router */
 routes.use('/newsAggregator', newsAggregatorRouter);
 
-/* CREATE and LISTEN Server */
-app.listen(PORT, (err) => {
-  if (!err) {
-    console.log(`Server listening on PORT: ${PORT}`);
-  } else {
-    console.log(`Some Error Enconuntered to run the Server: ${err}`);
-  }
-});
+/* CREATE and LISTEN Server (Running Server) */
+app
+  .listen(PORT, (error) => {
+    if (!error) {
+      console.log(MSG_SERVER_LISTING + PORT);
+    }
+  })
+  .on(ERROR_EVENT, (error) => {
+    console.error(ERR_SERVER_START + error);
+  });
