8d3eada5f2e3b251acd20fb33370f21ab7373499
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-12-14 22:34:06 +0530

    written test cases to validate the user registration request body

--- test/validators/userDetails.validator.test.js
+++ test/validators/userDetails.validator.test.js
@@ -1,39 +1,77 @@
 const chai = require('chai');
 const { expect } = chai;
 const chaiHttp = require('chai-http');
+// const usersDetails = require('../../src/db/users.json');
+const UserDetailsValidator = require('../../src/validators/userDetails.validator');
 
 chai.use(chaiHttp);
 
 describe('Validate User Details Request Details', function () {
+  let userDetail;
+  beforeEach((done) => {
+    userDetail = {
+      userName: 'TestName',
+      email: 'test@example.com',
+      password: 'test@mypassword',
+      role: 'admin',
+    };
+    done();
+  });
+
   it('1. Validate User Details Request Details: user details validated successfully', (done) => {
+    let response =
+      UserDetailsValidator.validateUserDetailsRequestInfo(userDetail);
+    expect(response.status).equal(true);
+    expect(response.statusCode).equal(200);
+    expect(response.message).equal('User details Validated successfully!');
     done();
   });
 
   it('2. Validate User Details Request Details: user details validation failed', (done) => {
+    delete userDetail.role;
+    let response =
+      UserDetailsValidator.validateUserDetailsRequestInfo(userDetail);
+    expect(response.status).equal(false);
+    expect(response.statusCode).equal(400);
+    expect(response.message).equal(
+      'Invalid used details! Something is missing!'
+    );
     done();
   });
 
-  it('3. Validate User Details Request Details: unique username validation successfully', (done) => {
+  it('3. Validate User Details Request Details: unique username validation failed', (done) => {
+    userDetail.userName = 'ShaniKGupta';
+    let response =
+      UserDetailsValidator.validateUserDetailsRequestInfo(userDetail);
+    expect(response.status).equal(false);
+    expect(response.statusCode).equal(403);
+    expect(response.message).equal(
+      'Username already in use. Please provide unique username!'
+    );
     done();
   });
 
-  it('4. Validate User Details Request Details: unique username validation failed', (done) => {
+  it('4. Validate User Details Request Details: user email validation failed', (done) => {
+    userDetail.email = 'test@@example.com';
+    let response =
+      UserDetailsValidator.validateUserDetailsRequestInfo(userDetail);
+    expect(response.status).equal(false);
+    expect(response.statusCode).equal(400);
+    expect(response.message).equal(
+      'Invalid email address! Please provide a valid email address'
+    );
     done();
   });
 
-  it('5. Validate User Details Request Details: user email validated successfully', (done) => {
-    done();
-  });
-
-  it('6. Validate User Details Request Details: user email validation failed', (done) => {
-    done();
-  });
-
-  it('7. Validate User Details Request Details: user role validated successfully', (done) => {
-    done();
-  });
-
-  it('8. Validate User Details Request Details: user role validation failed', (done) => {
+  it('5. Validate User Details Request Details: user role validation failed', (done) => {
+    userDetail.role = 'test';
+    let response =
+      UserDetailsValidator.validateUserDetailsRequestInfo(userDetail);
+    expect(response.status).equal(false);
+    expect(response.statusCode).equal(400);
+    expect(response.message).equal(
+      'Invalid user role. Please provide a valid role!'
+    );
     done();
   });
 });
